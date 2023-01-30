05738fef2267ca48b540ac8b350694edd99dea66
Just1pa <pageaud.justin@gmail.com>
2023-01-30 21:54:08 +0100

    Sponsoring

--- wp-content/themes/Cours-formation-wordpress/metaboxes/sponso.php
+++ wp-content/themes/Cours-formation-wordpress/metaboxes/sponso.php
@@ -13,15 +13,16 @@ class SponsoMetaBox{
 		add_meta_box(self::META_KEY,'Sponsoring',[self::class,'render'],'post','side');
 	}
 
-	public static function render(){
+	public static function render($post){
+		$value =get_post_meta($post->ID, self::META_KEY,true);
 		?>
 			<input type="hidden" value="0" name="<?php self::META_KEY?>">
-			<input type="checkbox" value="1" name="<?php self::META_KEY?>">
+			<input type="checkbox" value="1" name="<?php self::META_KEY?>"><?= $value==='1' ? 'checked' :''?>>
 			<label for="cours-sponso">Cet article est sponsorisé ! </label>
 		<?php
 	}
 
-	public static function save(){
+	public static function save($post){
 		if(array_key_exists(self::META_KEY, $_POST)&& current_user_can( 'edit_post',$post)){
 			if($_POST[self::META_KEY]==='0'){
 				delete_post_meta($post,self::META_KEY);
